ce24885a3369698d779e774a4c340ad2be8c2067
Wing <eamonn.kearns@so-4pt.net>
2023-03-27 05:09:11 +0100

    Added commenting and cleaned up the code a little.

--- solver.js
+++ solver.js
@@ -1,26 +1,48 @@
+/**
+ * A class to abstract the row, column and subgrid of a Sudoku Grid.
+ */
 class SudokuSpace
 {
+    /**
+     * The search space has 9 cells, these are SudokuCells
+     * @see SudokuCell
+     */
     cells;
-    found;
 
     constructor()
     {
+        // instantiate the array
         this.cells = [];
     }
 
+    /**
+     * Add a SudokuCell to the list of cells
+     * @param {SudokuCell} cell
+     */
     addCell(cell)
     {
         this.cells.push(cell);
     }
 
+    /**
+     * Fetch the SudokuCell from the given index
+     * @param {Number} index
+     * @returns {SudokuCell}
+     */
     getCell(index)
     {
         return this.cells[index];
     }
 
+    /**
+     * Run a search through the cells to see if, given the known cell values in the space, any new digits can be found.
+     * @returns {Number} returns the number of cells that have been changed by this pass.
+     */
     makePass()
     {
-        let [knownValues, unknownCells] = this.getValues();
+        let {knownValues, unknownCells} = this.getValues();
+        let changes = 0;
+
         for(let cell of this.cells)
         {
             if(cell.value)
@@ -32,36 +54,74 @@ class SudokuSpace
                 unknownCells.push(cell);
             }
         }
-        let changed = false;
+
         for(let cell of unknownCells)
         {
-            changed = changed || cell.makePass(knownValues);
+            changes += cell.makePass(knownValues);
         }
-        return changed;
+        return changes;
 
     }
 
+    /**
+     * Determine if there are any numbers which can go into only one cell. If that is the case, that cell must contain
+     * that value.
+     * @returns {boolean}  Returns true if there are any changes made, by this process.
+     */
+    checkForSingleAvailabilities()
+    {
+        let changed = false
+        let {missingValues} = this.getValues();
+        for(let [value, indices] of Object.entries(missingValues))
+        {
+            if(indices.length === 1)
+            {
+                this.cells[indices[0]].value = value;
+                changed = true;
+            }
+        }
+        return changed;
+    }
+
+    /**
+     * This method returns a dictionary of what we know about the search space. What cells have known values, what cells don't have known values, what numbers need to be found and what cells they can possibly go into.
+     * @returns Dictionary of values
+     */
     getValues()
     {
         let knownValues = [];
         let unknownCells = [];
-        for(let cell of this.cells)
+        let missingValues = {1: [], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[]};
+        for(let [key, cell] of Object.entries(this.cells))
         {
             if(cell.value)
             {
                 knownValues.push(cell.value);
+                delete(missingValues[cell.value]);
             }
             else
             {
                 unknownCells.push(cell);
+                for(let [value, possible] of Object.entries(cell.possibleValues))
+                {
+                    if(possible && missingValues[value])
+                    {
+                        missingValues[value].push(key);
+                    }
+                }
             }
         }
-        return [knownValues, unknownCells];
+
+        return {knownValues:knownValues, unknownCells:unknownCells, missingValues:missingValues};
     }
 
+    /**
+     * Makes sure a search space has not been incorrectly inputted by ensuring that each value exists only in one space.
+     * @returns {boolean}
+     */
     validate()
     {
-        let [knownValues, unknownCells] = this.getValues();
+        let {knownValues} = this.getValues();
         let uniqueKnownValues = knownValues.filter(onlyUnique);
         return uniqueKnownValues.length === knownValues.length;
     }
@@ -72,20 +132,33 @@ function onlyUnique(value, index, array)
     return array.indexOf(value) === index;
 }
 
+/**
+ * The class to represent the Sudoku Cell itself.
+ */
 class SudokuCell
 {
+    /**
+     * A hash map of booleans to store whether a given number can still be fit into the Cell
+     */
     possibleValues;
+    /**
+     * The value of the cell, whether provided or derived
+     */
     value;
 
     constructor(value)
     {
         this.possibleValues = {1: true, 2:true, 3:true, 4:true, 5:true, 6:true, 7:true, 8:true, 9:true}
-        if(value)
-        {
-            this.value = value;
-        }
+        this.value = value;
     }
 
+    /**
+     * This method takes the values that are known by the search space it belongs to and sets those values in this cell
+     * to false. If there is only one possible cell remaining, the value of this cell is set to that and the method
+     * returns true. Other-wise false.
+     * @param knownValues An array of values that this Cell can possibly contain
+     * @returns {boolean} Whether the cell was changed or not
+     */
     makePass(knownValues)
     {
         for(let value of knownValues)
@@ -110,6 +183,10 @@ class SudokuCell
     }
 }
 
+/**
+ * These two classes are just helper methods that I used in programming the solution part. They may be of further use,
+ * they may not.
+ */
 class SolvedSudokuCell extends SudokuCell
 {
     constructor(value)
@@ -126,28 +203,55 @@ class UnsolvedSudokuCell extends SudokuCell
     }
 }
 
+/**
+ * This class represents the grid in total. A game of sudoku is broken up into a nine by nine grid. Each row, column
+ * and subgrid of 3x3 must contain the numbers 1 - 9 once only.
+ */
 class Sudoku
 {
+    /**
+     * An array of SudokuSpaces
+     * @see SudokuSpace
+     */
     rows;
+    /**
+     * An array of SudokuSpaces
+     * @see SudokuSpace
+     */
     cols;
+    /**
+     * An array of SudokuSpaces
+     * @see SudokuSpace
+     */
     grids;
-    spaces;
 
+    /**
+     * Turns an array of numbers into a sudoku grid
+     * @param values A two-dimensional array of integers
+     */
     constructor(values)
     {
+        /*
+         * Instantiate the search spaces
+         */
         this.rows = [];
         this.cols = [];
         this.grids = [];
 
-        this.spaces = {rows:this.rows, cols:this.cols, grids:this.grids}
 
+        /*
+         * Populate the rows first. We'll use the rows to populate the grids and columns. This could be done at once,
+         * but honestly it's easier code to read if you do it in three separate steps.
+         */
         for(let i = 0; i < 9; i++)
         {
+            // instantiate the space
             let row = new SudokuSpace();
 
             for(let j = 0; j < 9; j++)
             {
                 let cell;
+                // if the cell has a value, create a solved cell, otherweise create an unsolved cell
                 if(values[i] && values[i][j])
                 {
                     cell = new SolvedSudokuCell(values[i][j])
@@ -156,11 +260,14 @@ class Sudoku
                 {
                     cell = new UnsolvedSudokuCell();
                 }
+                // add it to the row
                 row.addCell(cell);
             }
+            // add the row to the array
             this.rows.push(row);
         }
 
+        // populate the columns
         for(let i = 0; i < 9; i++)
         {
             let col = new SudokuSpace();
@@ -171,6 +278,7 @@ class Sudoku
             this.cols.push(col);
         }
 
+        //populate the grids
         let offset_i = 0;
         let offset_j = 0;
         for(let g = 0; g < 9; g++)
@@ -195,9 +303,36 @@ class Sudoku
         }
     }
 
+    /**
+     * This method isn't currently used but will turn this SudokuGrid into an array of numbers identical to the
+     * requirements of the constructor
+     */
+    toArray()
+    {
+        let values = [];
+        for(let row of this.rows)
+        {
+            let rowValues = [];
+            for(let cell of row.cells)
+            {
+                rowValues.push(
+                    cell.value?cell.value:''
+                );
+            }
+            values.push(rowValues);
+        }
+        return values;
+    }
+
+    /**
+     * Makes a basic pass through each of the search spaces to see if any of them have cells which have only one value
+     * based on the values of the other cells in the search space. If any changes are made at all, return true.
+     * @returns {boolean}
+     */
     makeBasicPass()
     {
         let changed = false;
+        // make a pass through all 9 rows, all 9 columns, and all 9 grids
         for(let i = 0; i < 9; i++)
         {
             let rowChanged = this.rows[i].makePass();
@@ -208,17 +343,46 @@ class Sudoku
         return changed;
     }
 
+    /**
+     * Check whether any single value cells have been derived.
+     * @returns {boolean} Returns true if any cells have their values changed
+     */
+    checkForSingleAvailabilities()
+    {
+        let changed = false;
+        for(let i = 0; i < 9; i++)
+        {
+            let rowChanged = this.rows[i].checkForSingleAvailabilities();
+            let colChanged = this.cols[i].checkForSingleAvailabilities();
+            let gridChanged = this.grids[i].checkForSingleAvailabilities();
+            changed = changed ||(rowChanged || colChanged || gridChanged);
+        }
+        return changed;
+    }
+
+    /**
+     * Get the cell at the given euclidian value.
+     * @param i the row to search
+     * @param j th cell to get
+     * @returns {SudokuCell|*}
+     */
     getCell(i, j)
     {
         return this.rows[i].getCell(j);
     }
 
+    /**
+     * Ensures the sudoku grid is valid. Returns a hash map with the valid field as true if the grid is fine.
+     * If the grid is not the valid field will be false, the index of any offending cell and whether the index refers
+     * to a row, column, or grid.
+     * @returns {{valid: boolean, index: number, space: string}|{valid: boolean}}
+     */
     validate()
     {
-        let valid = true;
+        let spaces = {rows:this.rows, cols:this.cols, grids:this.grids};
         for(let i = 0; i < 9; i++)
         {
-            for(let [toSearch, field] of Object.entries(this.spaces))
+            for(let [toSearch, field] of Object.entries(spaces))
             {
                 if(!field[i].validate())
                 {
@@ -230,13 +394,8 @@ class Sudoku
     }
 }
 
-function createEmptySudokuHTMLCell()
-{
 
-}
-
-
-//IIFE
+//IIFE encapsulating the UI
 (function($){
     function createUnFilledSudokuHTMLCell($container, cell)
     {
@@ -279,7 +438,7 @@ function createEmptySudokuHTMLCell()
             let row = rows[i];
             let cells = row.split('');
             let values = [];
-            if(cells.length != 9)
+            if(cells.length !== 9)
             {
                 valid = false;
             }
@@ -334,8 +493,13 @@ function createEmptySudokuHTMLCell()
         while(working)
         {
             working = solver.makeBasicPass();
-
+            if(!working)
+            {
+                working = solver.checkForSingleAvailabilities();
+            }
+            showUpdatedGrid();
         }
+
         showUpdatedGrid();
     }
 
@@ -348,7 +512,7 @@ function createEmptySudokuHTMLCell()
     // jquery document onload handler
     $(function(){
         $sudokuContainer = $('#sudokuContainer');
-        $sudokuInput = $('#sudokuValue').on("input", (e)=>{updateGrid();});
+        $sudokuInput = $('#sudokuValue').on("input", ()=>{updateGrid();});
         $solveButton = $('#solveButton').on('click', ()=>{solveSodoku();});
         buildSudokuHTML();
     });
